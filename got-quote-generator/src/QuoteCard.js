496c40adf9cfce9df61b3a08047a213e7fb73d57
Galina Atamankina <g.atamankina@gmail.com>
2025-08-20 11:24:36 +0200

    feat: Implemented the main structure of the QuoteCard component.

---
+++ got-quote-generator/src/QuoteCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function QuoteCard() {
+
+    return (
+        <blockquote>
+            "Das ist ein statisches Testzitat."
+            <footer>
+                - Test Charakter
+            </footer>
+        </blockquote>
+    )
+}
+
+export default QuoteCard;
