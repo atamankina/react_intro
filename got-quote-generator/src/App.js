099fdd569eeab3001181a0e5b2974839929453aa
Galina Atamankina <g.atamankina@gmail.com>
2025-08-20 11:52:56 +0200

    Refactoring quote card styles handling, moving styles from the App to the QuoteCard component.

--- got-quote-generator/src/App.js
+++ got-quote-generator/src/App.js
@@ -12,21 +12,6 @@ import QuoteCard from './QuoteCard';
     { id: 8, quote: "Valar Morghulis.", character: "Jaqen H'ghar", epic: true },
   ];
 
-  const quoteStyle = {
-    fontStyle: 'italic',
-    color: '#f89a03ff',
-    fontSize: '1.5em',
-    marginBottom: '10px',
-    borderLeft: '4px solid #f89a03ff',
-    paddingLeft: '15px'
-  };
-
-  const characterStyle = {
-    fontSize: '0.8em',
-    color: '#434343ff',
-    marginTop: '5px',
-    display: 'block'
-  };
 
 function App() {
   return (
@@ -42,8 +27,6 @@ function App() {
             quoteText={q.quote}
             characterName={q.character}
             isQuoteEpic={q.epic}
-            quoteStyle={quoteStyle}
-            characterStyle={characterStyle}
           />
         ))}
       </main>
